83b5e6781a64cc575488317d93f80c522e155e08
Hein Htoo <heinhtoozaw99@gmail.com>
2024-09-18 21:46:46 +0630

    fix route

--- lib/send.ts
+++ lib/send.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function sendVerificationRequest({ identifier: email, url }: any) {
   // Call the cloud Email provider API for sending emails
-  const response = await fetch("http://localhost:3000/api/send-email", {
+  const response = await fetch(process.env.NEXTAUTH_URL + "/api/send-email", {
     // The body format will vary depending on provider, please see their documentation
     body: JSON.stringify({
       email,
